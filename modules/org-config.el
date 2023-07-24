60707dea07aa9d6f7966c8577d7febc18eba244d
Mitch Labrenz <mitchell.labrenz@fileyourtaxes.com>
2023-07-24 16:51:19 -0700

    Update org roam directory

--- modules/org-config.el
+++ modules/org-config.el
@@ -1,7 +1,7 @@
 ;;; core-config.el -*- lexical-binding: t; -*-
 (use-package org-roam
   :init
-  (setq org-roam-directory (file-truename "~/documents/roam"))
+  (if (file-exists-p "~/documents/roam") (setq org-roam-directory (file-truename "~/documents/roam")))
   (setq org-roam-database-connector 'sqlite-builtin)
   (org-roam-db-autosync-mode t))
 
